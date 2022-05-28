47287bf03e04170f16215266ccd14140af508beb
ZimCanIT <77082071+ZimCanIT@users.noreply.github.com>
2022-05-28 14:27:11 +0100

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,25 +1,29 @@
 # install python 3.7 : https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/eb-cli3-install-linux.html 
 sudo yum update -y # update upstream package repositories 
-sudo yum install python37 -y
+sudo yum install git -y
 
-# install pip and python modules for scrpit
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
-pip install webdriver-manager
-pip install selenium
-pip install boto3 
 
-# installling google chrome for amazon linux: https://help.looker.com/hc/en-us/articles/360035411973-Installing-Chromium-for-Amazon-Linux
+# installling google chrome for amazon linux 
+# https://help.looker.com/hc/en-us/articles/360035411973-Installing-Chromium-for-Amazon-Linux
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 sudo yum install ./google-chrome-stable_current_x86_64.rpm -y
 sudo ln -s /usr/bin/google-chrome-stable /usr/bin/chromium
 
 
-# install git and clone code repo (edit code such that the username and password field read the ppw credentials as system variables)
-sudo yum install git -y
-git clone https://github.com/ZimCanIT/ec2_webscraper.git # need code in repo that uses boto3 mod to read credentials for signing into the site 
+python3 -m venv websScrapingProd/env
+source ~/websScrapingProd/env/bin/activate
+cd websScrapingProd
+python3 -m pip install --upgrade pip
 
-# change into the directory containing code
+
+# install modules for scrpit
+pip install webdriver-manager
+pip install selenium
+pip install boto3 
+
+git clone https://github.com/ZimCanIT/ec2_webscraper.git  
+
+# change into the directory containing code and run user_data.sh 
 cd ec2_webscraper 
-python 3.7 app.py # app needs to have an explicit wait defined 
+python app.py 
 
