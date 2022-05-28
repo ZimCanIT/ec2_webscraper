00ced9a3708f8d07b234394eff98e1d7066a5c33
ZimCanIT <77082071+ZimCanIT@users.noreply.github.com>
2022-05-28 18:31:01 +0100

    app.py
    Changed request deletion logic in delete_requests() method

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3 
 
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,7 +7,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import sys, param_store
+import param_store, time
+
 
 
 # login credentials read from parameter store
@@ -17,7 +17,7 @@ psswd = param_store.get_password()
 
 # chrome driver options 
 options = Options()
-options.headless = True # allows script to be run without a GUI on a server
+# options.headless = True # allows script to be run without a GUI on a server
 
 class SCRAPE:
 
@@ -43,36 +43,30 @@ class SCRAPE:
         smart_safe_acceptance = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="contentWrap"]/div[1]/form/div/input[1]'))).click()
         
 
+
     def delete_requests(self):
-        """Deletes friend requests awaiting approval and then signs out"""
+        """
+        Deletes friend requests awaiting approval and then signs out.
+        Closes the webdriver once all requests have been deleted
+        """
         driver = self.driver
 
-        for i in range(22): # 626/ 29 requests deleted per page
-            for cancel_button in range(29):
-                try:
-                    xpath = f'//*[@id="cancel{cancel_button}"]' 
-
-                    delete_req_button = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.XPATH, xpath)))
-                    delete_req_button.click()
-
-                except: 
-                    print("All requests have been deleted.")    
-                    sys.exit(1)
-
-                    
-    def close_driver(self):
-        """Closes the webdriver once all requests have been deleted"""
-        self.driver.quit()           
-            
+        while True: 
+            try: 
+                time.sleep(3) # explicit wait required as script kept failing after deleting a few requests. 
+                delete_req_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="cancel1"]'))).click() # iteratively selecting the same xpath
+                driver.get("https://www.penpalworld.com/friendsListAwait.asp") # reopens awaiting friend requests URL and refreshes the page after each delete request
+                driver.refresh()
+            except:
+                driver.quit()
             
 def main():
     run = SCRAPE(options=options)
     run.setup()
     run.site_navigation()
     run.delete_requests()
-    run.close_driver()
+   
 
-    
 if __name__ == "__main__":
     main()
 
