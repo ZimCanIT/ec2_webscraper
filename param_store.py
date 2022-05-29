e0fc9af215b87e29184f4543342997eb862dac55
ZimCanIT <77082071+ZimCanIT@users.noreply.github.com>
2022-05-29 16:04:58 +0100

    Update param_store.py

--- param_store.py
+++ param_store.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3 
 
-# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ssm/put-parameter.html
-# https://blair55.github.io/blog/aws-parameter-store-scripting/
-
 import boto3
 
 ssm_client = boto3.client("ssm", region_name="us-east-1")
